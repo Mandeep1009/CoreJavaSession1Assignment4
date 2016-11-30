03f7ab97eebc1684b849b568d5aab23f1ebb3b30
Mandeep1009 <yadavmandeep4@gmail.com>
2016-11-30 09:25:05 +0530

    Add files via upload

---
+++ ConvertASCII.java
@@ -0,0 +1,12 @@
+public class ConvertASCII 
+{
+ 
+     public static void main(String[] args) 
+	{
+         	for (int i = 65; i <= 90; i++) 
+		{
+             	System.out.println(i + "-" + (char) i);
+         	}
+     	}
+ 
+ }
